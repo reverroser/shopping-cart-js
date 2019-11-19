012a38f6a8841e1771546bda8af1f5468e749b7b
Roser Reverte <reverroser@gmail.com>
2019-11-19 16:23:06 +0100

    classes, js files and   html scripts corrected

---
+++ scripts/classes.js
@@ -0,0 +1,16 @@
+function ProductItems(name, details, img, price, properties, id) {
+    this.name = name;
+    this.details = details;
+    this.img = img;
+    this.price = price;
+    this.properties = properties;
+    this.id = id;
+
+}
+
+function Cart() {
+    this.products = [];
+
+    //make a function that adds products into cart
+    //make a function that removes products into cart
+}
