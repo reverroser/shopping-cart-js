ffc5ad2dc10d225f77aa239147546601143e6161
Roser Reverte <reverroser@gmail.com>
2019-11-23 23:36:53 +0100

    change data modal, update price by size

--- scripts/classes/productClass.js
+++ scripts/classes/productClass.js
@@ -4,17 +4,13 @@
  * @param {String} name The product´s name
  * @param {String} details The product´s details
  * @param {String} img The product´s img
- * @param {Array} price the product´s proce
- * @param {Array} stock The product´s stock amount
- * @param {Array} properties The product´s properties
+ * @param {Array} properties The product´s properties - [{ size, price, stock }]
  * @param {String} id The product´s id
  */
-function Product(name, details, img, price, stock, properties, id) {
+function Product(name, details, img, properties, id) {
     this.name = name;
     this.details = details;
     this.img = img;
-    this.price = price;
-    this.stock = stock;
     this.properties = properties;
     this.id = id;
 
--- scripts/classes/productsClass.js
+++ scripts/classes/productsClass.js
@@ -9,90 +9,85 @@ function Products() {
             'White rock',
             'lorem ipsum patatim',
             ['assets/whiterock.jpg'],
-            [1, 2, 3],
-            [1, 2, 3],
             [
                 {
-                    size: "big"
+                    price: {
+                        eur: 12,
+                        dol: 14,
+                    },
+                    size: 'big',
+                    stock: 10,
                 },
                 {
-                    size: "medium"
+                    price: {
+                        eur: 10,
+                        dol: 12,
+                    },
+                    size: 'medium',
+                    stock: 14,
                 },
                 {
-                    size: "small"
+                    price: {
+                        eur: 8,
+                        dol: 10,
+                    },
+                    size: 'small',
+                    stock: 10,
                 }
             ],
             '1'
-        ),
-        new Product(
-            'Gray rock',
-            'lorem ipsum patatim',
-            ['./assets/greyrock.jpg'],
-            [1, 2, 3],
-            [1, 2, 3],
-            [
-                {
-                    size: "big"
-                },
-                {
-                    size: "medium"
-                },
-                {
-                    size: "small"
-                }
-            ],
-            '2'
-        ),
-        new Product(
-            'Black rock',
-            'lorem ipsum patatim',
-            ['./assets/darkrock.jpg'],
-            [1, 2, 3],
-            [1, 2, 3],
-            [
-                {
-                    size: "big"
-                },
-                {
-                    size: "medium"
-                },
-                {
-                    size: "small"
-                }
-            ],
-            '3'
         ),
     ];
 
     // this function renders the content from the object product to the index page, dynamicaly.
     this.render = function () {
-        const productsGridEl = document.getElementById('productsGrid');
+        var productsGridEl = document.getElementById('productsGrid');
 
         this.list.forEach(function (product) {
-            const productEl = document.createElement('div');
+            var productEl = document.createElement('div');
             productEl.className = 'product-container';
 
-            const productImageEl = document.createElement('div');
+            var productImageEl = document.createElement('div');
             productImageEl.className = 'product-image';
             productImageEl.innerHTML = '<img src="' + product.img + '">';
 
             // This function stores the id of the product on localStorage, when the user clicks on the product-image.
-            productImageEl.onclick = function () {
-                localStorage.setItem('selected-product-id', product.id);
-                window.location.replace('product.html');
-            };
+            // productImageEl.onclick = function () {
+            //     localStorage.setItem('selected-product-id', product.id);
+            // };
 
-            const productNameEl = document.createElement('h2');
+            var productNameEl = document.createElement('h2');
             productNameEl.className = 'product-name';
             productNameEl.innerHTML = product.name;
 
-            const productPriceEl = document.createElement('h6');
+            // This variable selects the first item in the properties array as default.
+            var defaultProps = product.properties[0];
+
+            var productPriceEl = document.createElement('h6');
             productPriceEl.className = 'product-price';
-            productPriceEl.innerHTML = product.price;
+            productPriceEl.innerHTML = `${defaultProps.price.eur}€`;
+
+            // This is the size selector
+            var productSelectEl = document.createElement('select');
+            productSelectEl.id = `${product.id}-select`;
+            // Every time the size changes, the price is updated automatically.
+            productSelectEl.onchange = function () {
+                // Getting the property selected by its index.
+                var selectedProp = product.properties[productSelectEl.value];
+                productPriceEl.innerHTML = `${selectedProp.price.eur}€`;
+            };
+            for (var i = 0; i < product.properties.length; i++) {
+                var option = document.createElement('option');
+                // Saving the index in the value to get the prop selected on the onchange event.
+                option.value = i;
+                option.text = product.properties[i].size;
+                productSelectEl.appendChild(option);
+            }
 
             productEl.appendChild(productImageEl);
             productEl.appendChild(productNameEl);
             productEl.appendChild(productPriceEl);
+            productEl.appendChild(productSelectEl);
             productsGridEl.appendChild(productEl);
         });
     };
