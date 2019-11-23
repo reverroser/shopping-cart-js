250b1b0f46fac42f6df096ab816102918cb77fcd
Roser Reverte <reverroser@gmail.com>
2019-11-24 00:20:21 +0100

    add cart badge

--- scripts/classes/cartClass.js
+++ scripts/classes/cartClass.js
@@ -4,7 +4,9 @@
 function Cart() {
     this.products = JSON.parse(localStorage.getItem('cart')) || [];
     this.addProduct = function (product) {
+        var cartCountEl = document.getElementById('cartCount');
         // TODO: Add product selected props and save it in localStorage
         this.products.push(product);
+        cartCountEl.innerHTML = this.products.length;
     }
 }
