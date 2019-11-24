4776bb80199b7037491261ee6a0c761455a16c50
Roser Reverte <reverroser@gmail.com>
2019-11-24 01:01:04 +0100

    update badge on init

--- scripts/classes/cartClass.js
+++ scripts/classes/cartClass.js
@@ -4,17 +4,20 @@
 function Cart() {
     this.products = JSON.parse(localStorage.getItem('cart')) || [];
     this.addProduct = function (productId, properties) {
-        var cartCountEl = document.getElementById('cartCount');
         this.products.push({
             id: productId,
             properties,
         });
         localStorage.setItem('cart', JSON.stringify(this.products));
-        cartCountEl.innerHTML = this.products.length;
+        this.updateBadge();
     }
     this.getSelectedProductsById = function (id) {
         return this.products.filter(function (product) {
             return product.id === id;
         });
+    }
+    this.updateBadge = function () {
+        var cartCountEl = document.getElementById('cartCount');
+        cartCountEl.innerHTML = this.products.length;
     }
 }
